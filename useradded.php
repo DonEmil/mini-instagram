16d2b1b0a18ca57b8398a9a52b8b1c9cd75b9a07
MarkusBerthold <mbert14@student.aau.dk>
2017-04-24 12:23:32 +0200

    Check for if email is already in use

--- useradded.php
+++ useradded.php
@@ -30,7 +30,14 @@ if(isset($_POST['submit'])){
  
         // Trim white space from the email and store the email
         $_email = trim($_POST['email']);
- 
+        $_check_email = $mysqli->query("SELECT * FROM users WHERE email='$_email'") // or die($mysqli->error());
+        
+        // We know email exists if the rows returned are more than 0
+        if ($_check_email->num_rows > 0) {
+            echo 'A user with ' . $_email . ' alredy exists<br />';
+            //I add the email to the list of missing data, so that the user has to add another.
+            $data_missing[] = 'E-mail';
+        } 
     }
 
     if(empty($_POST['password'])){
