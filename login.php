114772e1ed876abc8548cac78af41eb62aecb09d
emil <emil.digit@gmail.com>
2017-04-25 11:46:00 +0200

    Login works -ish

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         <div id="fullscreen_bg" class="fullscreen_bg"/>
         
         <?php
-        if (isset($_POST['submit'])) {
+        if (isset($_POST['login'])) {
 
             $data_missing = array();
 
@@ -26,6 +26,16 @@
                 // We know username exists if the rows returned are more than 0
                 if ($_check_username->num_rows > 0) {
                     // great, a user exists with the given username
+                    $_user = $_check_username->fetch_assoc();
+                    if(password_verify($_POST['password'], $_user['password'])){
+
+                        header("location: main.html");
+
+                    } else {
+
+                        echo '<span style="color:#FF0000;text-align:center;">Incorrect password<br /></span>';
+                        $data_missing[] = 'Password';
+                    }
                 } else {
                     echo '<span style="color:#FF0000;text-align:center;">A user does not exist with this username<br /></span>';                        
                     $data_missing[] = 'Username';
@@ -37,62 +47,20 @@
                 // Adds password to array
                 $data_missing[] = 'Password';
 
-            } else {
-
-                // Trim white space from the password and store the password
-                $_password = trim($_POST['password']);
             }
 
 
-            if (/*if password is in the same entry as the username*/) {
-                //success
-            } else {
-                echo '<span style="color:#FF0000;text-align:center;">provided username and password do not match<br /></span>'; 
-            }
-
-            if (empty($data_missing)) {
-
-                $query = "INSERT INTO users (username, email, password, confirm_password) VALUES (?, ?, ?, ?)";
-
-                $stmt = mysqli_prepare($dbc, $query);
-
-                mysqli_stmt_bind_param($stmt, "ssss", $_username, $_email, $_password, $_confirm_password);
-
-                mysqli_stmt_execute($stmt);
-
-                $affected_rows = mysqli_stmt_affected_rows($stmt);
-
-                if ($affected_rows == 1) {
-
-                    echo 'User Entered';
-
-                    mysqli_stmt_close($stmt);
-
-                    mysqli_close($dbc);
-
-                    header("Location: main.html");
-                    die();
-                } else {
-
-                    echo 'Error Occurred<br />';
-                    echo mysqli_error();
-
-                    mysqli_stmt_close($stmt);
-
-                    mysqli_close($dbc);
-                }
-            } else {
-
+            if (!empty($data_missing)) {
                 echo '<span style="color:#FF0000;text-align:center;">You need to enter the following data correctly<br /></span>';                
 
                 foreach ($data_missing as $missing) {
                     echo '<span style="color:#FF0000;text-align:center;">' . $missing . '<br /></span>';
                 }
-            }
+            } 
         }
         ?>
         
-        <form action="http://localhost/mini-instagram/login.html">
+        <form action="http://localhost/mini-instagram/index.html">
         <input type="submit" value="Go back to login page" />
         </form>
     </body>
